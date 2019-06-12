be834b42e4295ddbe251b91d1d74287c33790920
Nate Kinne <nathan.kinne@gmail.com>
2019-06-11 21:50:38 -0500

    state census data

--- code.js
+++ code.js
@@ -1,15 +1,26 @@
 // census api for population stats
-function census(){
-    fetch('https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE=7&for=state:01&key=915200b4a6a1702ea363d22e5199819a1224f052')
+function censusState(term){
+    fetch('https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE=7&for=state:*&key=915200b4a6a1702ea363d22e5199819a1224f052')
         .then(response => response.json())
-        .then(responseJson => displayCensusStats(responseJson))
+        .then(responseJson => displayCensusStats(responseJson, term))
         .catch(error => alert("An error occured! Please try again later"));
 }
 
 // display api stats
-function  displayCensusStats(response){
-    console.log(response);
+function displayStateCensus(response, term){ 
+    let exists = false;
+    response.forEach(function(found){
+        if(found[0] === term){
+            console.log(found + " found it finally");
+            $('.location').text(term);
+            $('.state').text(found[1]); 
+            exists = true;
+        }
+    });
+    if(!exists) console.log('doesnt exist');
 }
+
+
 
 // get data from form
 function watchForm(){
@@ -20,12 +31,13 @@ function watchForm(){
         let searchType = $("#searches").val();
         // search term
         let term = $("#term").val();
-        console.log(searchType);
-        console.log(term);
+        // show hidden items
+        $('.non-default').removeClass('hidden');
         if (searchType === "City") {
             console.log(searchType);
         } else if (searchType === "State") {
-           census();
+           $('.state').removeClass('hidden'); 
+           censusState(term);
         } else if (searchType === "County") {
             console.log(searchType);
         }
